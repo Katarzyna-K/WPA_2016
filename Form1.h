6df866df01457ff7ad10e7368af6fd51a9477445
Katarzyna-K <kkozlowskaster@gmail.com>
2016-05-12 14:17:35 +0200

    dodano koniec

--- Form1.h
+++ Form1.h
@@ -36,6 +36,19 @@ namespace WindowsFormApplication1 {
 		}
 	private: System::Windows::Forms::Button^  button1;
 	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  plikToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja1ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja2ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  wyjdzToolStripMenuItem;
+
+	private: System::Windows::Forms::ToolStripMenuItem^  edycjaToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja3ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja31ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja32ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcja4ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  oProgramieToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  oAutorzeToolStripMenuItem;
 	protected:
 
 	private:
@@ -51,9 +64,21 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->plikToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->edycjaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->oProgramieToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja1ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja2ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->wyjdzToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja3ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja4ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja31ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcja32ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->oAutorzeToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
@@ -81,6 +106,98 @@ namespace WindowsFormApplication1 {
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->GripStyle = System::Windows::Forms::ToolStripGripStyle::Visible;
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->plikToolStripMenuItem,
+					this->edycjaToolStripMenuItem, this->oProgramieToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(923, 24);
+			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// plikToolStripMenuItem
+			// 
+			this->plikToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->opcja1ToolStripMenuItem,
+					this->opcja2ToolStripMenuItem, this->wyjdzToolStripMenuItem
+			});
+			this->plikToolStripMenuItem->Name = L"plikToolStripMenuItem";
+			this->plikToolStripMenuItem->Size = System::Drawing::Size(38, 20);
+			this->plikToolStripMenuItem->Text = L"Plik";
+			// 
+			// edycjaToolStripMenuItem
+			// 
+			this->edycjaToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->opcja3ToolStripMenuItem,
+					this->opcja4ToolStripMenuItem
+			});
+			this->edycjaToolStripMenuItem->Name = L"edycjaToolStripMenuItem";
+			this->edycjaToolStripMenuItem->Size = System::Drawing::Size(53, 20);
+			this->edycjaToolStripMenuItem->Text = L"Edycja";
+			// 
+			// oProgramieToolStripMenuItem
+			// 
+			this->oProgramieToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->oAutorzeToolStripMenuItem });
+			this->oProgramieToolStripMenuItem->Name = L"oProgramieToolStripMenuItem";
+			this->oProgramieToolStripMenuItem->Size = System::Drawing::Size(86, 20);
+			this->oProgramieToolStripMenuItem->Text = L"O programie";
+			// 
+			// opcja1ToolStripMenuItem
+			// 
+			this->opcja1ToolStripMenuItem->Name = L"opcja1ToolStripMenuItem";
+			this->opcja1ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja1ToolStripMenuItem->Text = L"Opcja 1";
+			// 
+			// opcja2ToolStripMenuItem
+			// 
+			this->opcja2ToolStripMenuItem->Name = L"opcja2ToolStripMenuItem";
+			this->opcja2ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja2ToolStripMenuItem->Text = L"Opcja 2";
+			// 
+			// wyjdzToolStripMenuItem
+			// 
+			this->wyjdzToolStripMenuItem->Name = L"wyjdzToolStripMenuItem";
+			this->wyjdzToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->wyjdzToolStripMenuItem->Text = L"WyjdŸ";
+			// 
+			// opcja3ToolStripMenuItem
+			// 
+			this->opcja3ToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->opcja31ToolStripMenuItem,
+					this->opcja32ToolStripMenuItem
+			});
+			this->opcja3ToolStripMenuItem->Name = L"opcja3ToolStripMenuItem";
+			this->opcja3ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja3ToolStripMenuItem->Text = L"Opcja 3";
+			// 
+			// opcja4ToolStripMenuItem
+			// 
+			this->opcja4ToolStripMenuItem->Name = L"opcja4ToolStripMenuItem";
+			this->opcja4ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja4ToolStripMenuItem->Text = L"Opcja 4";
+			// 
+			// opcja31ToolStripMenuItem
+			// 
+			this->opcja31ToolStripMenuItem->Name = L"opcja31ToolStripMenuItem";
+			this->opcja31ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja31ToolStripMenuItem->Text = L"Opcja 3.1";
+			// 
+			// opcja32ToolStripMenuItem
+			// 
+			this->opcja32ToolStripMenuItem->Name = L"opcja32ToolStripMenuItem";
+			this->opcja32ToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->opcja32ToolStripMenuItem->Text = L"Opcja 3.2";
+			// 
+			// oAutorzeToolStripMenuItem
+			// 
+			this->oAutorzeToolStripMenuItem->Name = L"oAutorzeToolStripMenuItem";
+			this->oAutorzeToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->oAutorzeToolStripMenuItem->Text = L"O autorze";
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
@@ -88,9 +205,14 @@ namespace WindowsFormApplication1 {
 			this->ClientSize = System::Drawing::Size(923, 261);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"Form1";
 			this->Text = L"Têczowe okienko";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -98,6 +220,9 @@ namespace WindowsFormApplication1 {
 				 Close();
 				 //label1->Text = "Zabiles go"
 	}
+			 private: System::Void wyjdzToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e){
+
+			 }
 	};
 }
 
